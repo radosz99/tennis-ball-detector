07d62d84bde2a424ae657f6910114170570b3b7e
Radosław Lis <47248979+radosz99@users.noreply.github.com>
2021-04-04 16:46:49 +0200

    Update picture_generator.py

--- picture_generator.py
+++ picture_generator.py
@@ -7,15 +7,18 @@ import shutil
 # path = 'https://picsum.photos/200/200'
 path = 'https://source.unsplash.com/random/200x200'
 
+
 def get_number_of_files_in_directory(dir_name):
     path, dirs, files = next(os.walk(dir_name))
     return len(files)
+
 
 def save_random_grayscale_image(image_path):
     urllib.request.urlretrieve(path, image_path)
     img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     cv2.imwrite(image_path, img)
 
+    
 def check_if_image_not_exists(image_path):
     image = cv2.imread(image_path)
     listdir_original = os.listdir('neg')
@@ -26,6 +29,7 @@ def check_if_image_not_exists(image_path):
             original = False
             break
     return original
+
 
 def save_images():
     if(not os.path.exists('tmp')):
@@ -41,8 +45,7 @@ def save_images():
             print(f"Picture added to directory, currently {id + 1} negative pictures")
             shutil.copyfile(image_path, f"neg/{str(id + 1)}.jpg")
         else:
-            print(f"Picture already in directory, currently {id + 1} negative pictures")
-
+            print(f"Picture already in directory, currently {id} negative pictures")
 
 
 save_images()
